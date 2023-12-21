8caa5eddf8e1308a896b67e669dfa0da0e99e32e
Kunyelewosi Alexander <kunalexander360@gmail.com>
2023-12-21 15:13:24 +0000

    Initial commit

---
+++ random_password_generator.py
@@ -0,0 +1,8 @@
+"""
+# Ask the user if the want to generate a password or not
+# If yes, ask for password length
+# generate the password
+# Print password
+# If initial password is no exit the program
+"""
+
